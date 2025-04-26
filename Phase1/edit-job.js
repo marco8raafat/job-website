7c990d0cbeeb46914302c6374db4a77f941528db
marco8raafat <marco782005@gmail.com>
2025-04-26 03:45:34 +0300

    marco

--- Phase1/edit-job.js
+++ Phase1/edit-job.js
@@ -40,7 +40,24 @@ document.addEventListener("DOMContentLoaded", function () {
     const updatedJobs = jobs.map((j) => (j.id == jobId ? job : j));
     localStorage.setItem("jobs", JSON.stringify(updatedJobs));
 
-    alert("Job updated successfully!");
-    window.location.href = "company-jobs.html";
+    // alert("Job updated successfully!");
+    showToast("Job updated successfully!", "success");
+    setTimeout(() => {
+      window.location.href = "company-jobs.html";
+  }, 2000);
   });
 });
+
+function showToast(message, type = 'success') {
+  const toast = document.getElementById('toast');
+  toast.textContent = message;
+  toast.style.backgroundColor = 
+      type === 'error' ? 'var(--danger-color)' :
+      type === 'warning' ? 'var(--danger-color)' :
+      'var(--success-color)';
+  toast.style.display = 'block';
+  
+  setTimeout(() => {
+      toast.style.display = 'none';
+  }, 3000);
+}
